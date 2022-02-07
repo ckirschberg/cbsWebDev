9659fdc0e2684851830b7c2e26f2c1dc74ff6bc7
ckirschberg <ckirschberg@gmail.com>
2022-02-07 15:53:14 +0100

    todo

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
+import { Todo } from './Todo';
 
 export default function App() {
+  const [text, setText] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const handleAddTodo = () => {
+    const todo = new Todo(Math.random().toString(), text);
+
+    setTodos(oldTodos => [...oldTodos, todo])
+    console.log(todos);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! How is this all working?</Text>
+
+      <TextInput placeholder="Add your todo here" value={text}
+        onChangeText={newText => setText(newText)} style={styles.borderBlack} />
+
+      <Button title="Add todo" onPress={handleAddTodo} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +31,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundolor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  borderBlack: {
+    border: '1px solid #000'
+  }
 });
---
+++ Todo.js
@@ -0,0 +1,6 @@
+export class Todo {
+    constructor(id, text) {
+        this.id = id;
+        this.text = text;
+    }
+}
