6a4141f3a706cbc82926a13ab08f8e868b97fba5
ckirschberg <ckirschberg@gmail.com>
2022-03-21 14:56:29 +0100

    .

--- store/actions/ChatActions.js
+++ store/actions/ChatActions.js
@@ -23,3 +23,28 @@ export const addChatroom = (chatroomName) => {
 export const deleteChatroom = (title) => {
     return { type: DELETE_CHATROOM, payload: title }
 }
+
+export const signup = (email, password) => {
+    return async dispatch => {
+        const response = await fetch('some url', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ //javascript to json
+                //key value pairs of data you want to send to server
+                // ...
+
+                returnSecureToken: true
+            })
+        });
+
+        const data = await response.json(); // json to javascript
+        console.log(data);
+        if (!response.ok) {
+            //There was a problem..
+        } else {
+            dispatch({ type: SIGNUP, payload: 'something to pass to reducer' })
+        }
+    };
+};
