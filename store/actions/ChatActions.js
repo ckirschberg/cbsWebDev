6950c356fe3b4486d0dbee9feaaabf456158fd95
ckirschberg <ckirschberg@gmail.com>
2022-03-28 14:59:46 +0200

    .

--- store/actions/ChatActions.js
+++ store/actions/ChatActions.js
@@ -5,6 +5,7 @@ export const ADD = 'ADD';
 export const SUBTRACT = 'SUBTRACT';
 export const ADD_CHATROOM = 'ADD_CHATROOM';
 export const DELETE_CHATROOM = 'DELETE_CHATROOM';
+export const FETCH_CHATROOMS = 'FETCH_CHATROOMS';
 
 export const toggleHappy = () => {
     return { type: TOGGLE_HAPPY };
@@ -17,6 +18,39 @@ export const add = () => {
 export const subtract = () => {
     return { type: SUBTRACT };
 };
+
+export const fetchChatrooms = () => {
+    return async (dispatch, getState) => {
+        const idToken = getState().user.idToken
+
+        const response = await fetch(
+            'https://cbsstudentsweb-default-rtdb.europe-west1.firebasedatabase.app/chatrooms.json?auth='
+            + idToken, {
+
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+
+        const data = await response.json(); // json to javascript
+        console.log(data);
+        if (!response.ok) {
+            //There was a problem..
+        } else {
+
+            let chatrooms = [];
+            for (const key in data) {
+                let chatroom = new Chatroom(data[key].chatroomName, [], '', blabla)
+                chatrooms.push()
+                // console.log(data[key].name)​
+            }
+
+            dispatch({ type: FETCH_CHATROOMS, payload: '' })
+        }
+    };
+}
 
 export const addChatroom = (chatroomName) => {
     return async (dispatch, getState) => {
